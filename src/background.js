973b332ad14420d6793e77735115d5ce1997a860
maddy <maddyvarner@gmail.com>
2014-03-21 12:47:35 -0400

    init commit

---
+++ src/background.js
@@ -0,0 +1,41 @@
+var probability = 0;
+
+chrome.tabs.onCreated.addListener( function(tab) {
+	currentTabId = tab.id;
+	if (Math.floor((Math.random()*10)+1) <= probability) {
+		chrome.windows.getAll({populate:true}, function(windowList) {
+			for (var i = 0; i < windowList.length; i++) {
+				for (var j = 0; j < windowList[i].tabs.length; j++) {
+					tabId = windowList[i].tabs[j].id
+					if (tabId != currentTabId) {
+						try {
+    						chrome.tabs.remove(tabId, function() {
+      							appendToLog('tab: ' + tabId + ' removed.');
+    						});
+  						} catch (e) {
+  							alert(e);
+  						}
+					}
+				}
+			}
+		});
+		probability = 0; 
+	}
+	probability = (probability + 1) % 10;
+});
+
+chrome.tabs.onRemoved.addListener( function(tab) {
+	if (probability > 0) {
+		probability--;
+	}
+});
+
+/*function removeWindow(var id) {
+	try {
+    	chrome.windows.remove(windowList[i].id, function() {
+      		appendToLog('window: '+windowList[i].id+' removed.');
+    	});
+  	} catch (e) {
+    	alert(e);
+  	}
+};*/
